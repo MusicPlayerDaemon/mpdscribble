493b222adff650dccbdaa983572e6d48bbfd7bdd
Max Kellermann <max@duempel.org>
2008-12-24 01:25:29 +0100

    as: fall back to libgcrypt for MD5 hashing
    The MD5 checksum functions were introduced in GLib 2.16.  To allow
    users of older GLib versions to use mpdscribble, fall back to
    libgcrypt.  libgcrypt is used by libsoup anyway.

--- src/as.c
+++ src/as.c
@@ -27,6 +27,10 @@
 
 #include <glib.h>
 
+#if GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION < 16
+#include <gcrypt.h>
+#endif
+
 #include <assert.h>
 #include <stdbool.h>
 #include <ctype.h>
@@ -348,7 +352,27 @@ static char *as_md5(const char *password, const char *timestamp)
 	char *cat, *result;
 
 	cat = g_strconcat(password, timestamp, NULL);
+#if GLIB_MAJOR_VERSION > 3 || (GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION >= 16)
 	result = g_compute_checksum_for_string(G_CHECKSUM_MD5, cat, -1);
+#else
+	/* fall back to libgcrypt on GLib < 2.16 */
+	{
+		gcry_md_hd_t hd;
+		unsigned char *p;
+		size_t i;
+
+		if (gcry_md_open(&hd, GCRY_MD_MD5, 0) != GPG_ERR_NO_ERROR)
+			g_error("gcry_md_open() failed\n");
+		gcry_md_write(hd, cat, strlen(cat));
+		p = gcry_md_read(hd, GCRY_MD_MD5);
+		if (p == NULL)
+			g_error("gcry_md_read() failed\n");
+		result = g_malloc(gcry_md_get_algo_dlen(GCRY_MD_MD5) * 2 + 1);
+		for (i = 0; i < gcry_md_get_algo_dlen(GCRY_MD_MD5); ++i)
+			snprintf(result + i * 2, 3, "%02x", p[i]);
+		gcry_md_close(hd);
+	}
+#endif
 	g_free(cat);
 
 	return result;
