9a58c479973548a156280afe193ee076337312ab
Max Kellermann <max@duempel.org>
2008-12-25 02:20:39 +0100

    cmdline: don't print AS client id in --version

--- src/cmdline.c
+++ src/cmdline.c
@@ -19,17 +19,17 @@
 
 #include "cmdline.h"
 #include "file.h"
-#include "as.h"
 #include "config.h"
 
 #include <glib.h>
 
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static const char *blurb =
-	"mpdscribble (" AS_CLIENT_ID " " AS_CLIENT_VERSION ").\n"
+	PACKAGE " version " VERSION "\n"
 	"another audioscrobbler plugin for music player daemon.\n"
 	"Copyright 2005,2006 Kuno Woudt <kuno@frob.nl>.\n"
 	"Copyright 2008 Max Kellermann <max@duempel.org>\n" "\n";
